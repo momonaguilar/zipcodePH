ad1a35ed7e3d0bc40fb37f7638a07226355e5abc
Jose @ GCP <jose.for.dev@gmail.com>
2022-01-20 15:28:21 +0000

    Add viper for env variable config; refactor zipcode logic

--- main.go
+++ main.go
@@ -9,46 +9,57 @@ import (
 	"os"
 	"strings"
 	"time"
+
+	"github.com/spf13/viper"
 )
 
 type ZipCode struct {
-	Zipcode      string
-	Area         string
-	ProvinceCity string
+	Zipcode      string `json:"zipcode"`
+	Area         string `json:"area"`
+	ProvinceCity string `json:"provinceCity"`
 }
 
 func main() {
 	http.HandleFunc("/zipcode", handleGetZip)
-	fmt.Println("Starting web server at port 8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
-		log.Fatal(err)
+
+	viper.AddConfigPath(".")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("ERROR: Error reading config file. Err:", err)
 	}
+	viper.SetDefault("APP.PORT", "8081")
+	PORT, ok := viper.Get("APP.PORT").(string)
+
+	if !ok {
+		log.Fatalln("ERROR: Invalid type assertion")
+	}
+
+	fmt.Println("INFO: Starting web server at port", PORT)
+	if err := http.ListenAndServe(":"+PORT, nil); err != nil {
+		log.Fatalln("ERROR: Unable to listen and serve, Err:", err)
+	}
+
 }
 
 func handleGetZip(rw http.ResponseWriter, r *http.Request) {
-	//time start
 	start := time.Now()
-
-	fmt.Fprintln(rw, "INFO: Starting")
 
 	if r.URL.Path != "/zipcode" {
 		http.Error(rw, "404 not found", http.StatusNotFound)
 		return
 	}
 
-	fmt.Fprintln(rw, "INFO: Starting")
-
 	if r.Method != "GET" {
 		http.Error(rw, "Method not supported.", http.StatusNotFound)
 		return
 	}
 
-	fmt.Fprintln(rw, "INFO: Starting")
-
 	key := r.FormValue("key")
 
 	//open file
-	f, err := os.Open("zipcode.csv")
+	f, err := os.Open("data/zipcode.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -56,17 +67,14 @@ func handleGetZip(rw http.ResponseWriter, r *http.Request) {
 	//close when program ends
 	defer f.Close()
 
-	fmt.Fprintln(rw, "INFO: Starting")
-
 	//read values
 	csvReader := csv.NewReader(f)
 	data, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintln(rw, key)
-	fmt.Fprintln(rw, "INFO: Starting...")
-	var resultZipcode []ZipCode
+
+	resultZipcode := []ZipCode{}
 
 	for _, line := range data {
 		if line[0] == key || strings.EqualFold(line[1], key) {
@@ -79,14 +87,16 @@ func handleGetZip(rw http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	fmt.Fprintln(rw, "INFO: Starting")
 	result, err := json.Marshal(resultZipcode)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Fprintln(rw, string(result))
 
 	elapsed := time.Since(start)
-	fmt.Println("Done after: ", elapsed.Milliseconds(), "ms")
+
+	logStr := "Key:" + key + ", Result:" + string(result) + ". Completed after " + fmt.Sprint(elapsed.Milliseconds()) + "ms"
+	fmt.Println("INFO:", logStr)
 
 }
